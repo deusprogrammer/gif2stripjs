9f7224c1db1a8e3c3237a20d314b677c61705969
Michael Main <deusprogrammer@gmail.com>
2021-11-11 04:05:30 -0600

    Added comments and create temp directory for you

--- src/index.js
+++ src/index.js
@@ -1,18 +1,27 @@
 const extractFrames = require('gif-extract-frames');
 const ji = require('join-images');
+const fs = require('fs');
 
 (async () => {
+    // Create temp directory
+    if (!fs.existsSync('./temp')){
+        fs.mkdirSync('./temp');
+    }
+
+    // Extract frames
     const results = await extractFrames({
         input: process.argv[2],
         output: './temp/frame-%d.png',
         coalesce: false
     });
 
+    // Add frames to list
     let images = [];
     for (let i = 0; i < results.shape[0]; i++) {
         images.push(`./temp/frame-${i}.png`);
     }
     
+    // Merge together into one image
     ji.joinImages(images, {
         direction: "horizontal",
         color: { alpha: 0, b: 0, g: 0, r: 0 }
